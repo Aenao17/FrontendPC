afa1712cf6666107c73a52029d5f6cb25cdaccfd
gabrieloanea17 <gabrieloanea17@gmail.com>
2024-11-13 18:21:23 +0200

    login/signup done

---
+++ frontendIonicAngular/src/app/pages/add-event/add-event.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+
+@Component({
+  selector: 'app-add-event',
+  templateUrl: './add-event.page.html',
+  styleUrls: ['./add-event.page.scss'],
+})
+export class AddEventPage implements OnInit {
+
+  constructor(private auth: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+
+  addEvent(){
+
+  }
+
+}
