a9a6a067eadc757a0c67ac93232717351ca6e205
gabrieloanea17 <gabrieloanea17@gmail.com>
2024-11-13 02:43:24 +0200

    merge login/signup

--- frontendIonicAngular/src/app/services/auth.service.ts
+++ frontendIonicAngular/src/app/services/auth.service.ts
@@ -1,16 +1,46 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'https://localhost:8080'; // Adjust API URL
+
+  private apiUrl = 'http://localhost:8080/api/users'; // Adjust API URL
 
   constructor(private http: HttpClient) {}
 
   login(username: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, { username, password });
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    const body = {
+      id: 1,
+      role: 'USER',
+      username: username,
+      password: password,
+      organizedEvents: [],
+      comments: []
+    }
+    return this.http.post(`${this.apiUrl}/login`, body);
+  }
+
+  signup(username: string, password: string): Observable<any> {
+    //make a signup request to the server
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    const body = {
+      id: 1,
+      role: 'USER',
+      username: username,
+      password: password,
+      organizedEvents: [],
+      comments: []
+    };
+    return this.http.post(`${this.apiUrl}/signup`, body );
   }
 }
